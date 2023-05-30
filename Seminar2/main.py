0b631fd2e20cbea10d712c0c274d32da0fe947aa
Jaha <zlgsbazarbaev@gmail.com>
2023-05-30 15:20:58 +0500

    changes in seminar2

--- Seminar2/main.py
+++ Seminar2/main.py
@@ -127,6 +127,21 @@
 #         count += 1
 # print(count)
 
+# Идеальное решение
+# n = int(input())
+# count_zero = 0
+# count_one = 0
+# for i in range(n):
+# x = int(input())
+# if x == 0:
+# count_zero += 1
+# else:
+# count_one += 1
+# if count_one > count_zero:
+# print(count_zero)
+# else:
+# print(count_one)
+
 
 
 # Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя –
@@ -149,6 +164,14 @@
 #             print(*sorted([i, j]))
 #             break
 
+# Идеальное решение
+
+# x = int(input())
+# y = int(input())
+# for i in range(x):
+#     for j in range(y):
+#         if x == i + j and y == i * j:
+#             print(i, j)
 
 
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа
@@ -165,3 +188,11 @@
 #         else:
 #             m = m * 2
 #             print(m)
+
+# Идеальное решение
+
+# n = int(input())
+# i = 0
+# while 2 ** i <= n:
+#     print(2 ** i)
+#     i += 1
