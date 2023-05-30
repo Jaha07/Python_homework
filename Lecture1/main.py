930878eba70477c90a84d63526dd22fa6a8a96ce
Jaha <zlgsbazarbaev@gmail.com>
2023-05-30 15:23:35 +0500

    123

---
+++ Lecture1/main.py
@@ -0,0 +1,45 @@
+# iter = 2
+# iter += 3 # iter = iter + 3
+# iter -= 4 # iter = iter - 4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5 (целочисленное деление)
+# iter %= 5 # iter = iter % 5
+# iter **= 5 # iter = iter ** 5 (возведение в степень)
+
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+
+
+# text = 'СъЕШЬ ещё этих МяГкИх французских булок'
+# print(len(text))
+# print('ещё' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('ещё', 'ЕЩЁ'))
+
+
+# text = 'съешь ещё этих мягких французских булок'
+# print(text[0]) # c
+# print(text[1]) # ъ
+# print(text[len(text)-1]) # к
+# print(text[-1]) #к
+# print(text[-5]) # б
+# print(text[:]) # съешь ещё этих мягких французских булок
+# print(text[:2]) # съ
+# print(text[len(text)-2:]) # ок
+# print(text[2:9]) # ешь ещё
+# print(text[6:-18]) # ещё этих мягких
+# print(text[0:len(text):6]) # сеикакл
+# print(text[::6]) # сеикакл
+# text = text[2:9] + text[-5] + text[:2] # ...
